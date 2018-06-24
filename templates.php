c600fe8ac4d30a720f4f20caae44a12ee548a3b8
jonhartm <jonhartm@umich.edu>
2018-06-24 19:33:18 -0400

    added simple css and feedback to TF/SA templates

--- templates.php
+++ templates.php
@@ -6,7 +6,13 @@
     {{else}}
         <i class="fa fa-times text-danger"></i>
     {{/if}} {{/if}}
-    {{{question}}}</p>
+    {{{question}}}
+    {{#if scored}}{{#if feedback}}
+      <span class={{#if correct}}feedback_correct{{else}}feedback_incorrect{{/if}}>
+        {{{feedback}}}
+      </span>
+    {{/if}}{{/if}}
+    </p>
     <p><input type="text" name="{{code}}" value="{{value}}" size="80"/></p>
   </li>
 </script>
@@ -29,12 +35,20 @@
 <script id="true_false_question" type="text/x-handlebars-template">
   <li>
     <p>
-    {{#if scored}}{{#if correct}}
-        <i class="fa fa-check text-success"></i>
-    {{else}}
-        <i class="fa fa-times text-danger"></i>
-    {{/if}} {{/if}}
-    {{{question}}}</p>
+      {{#if scored}}{{#if correct}}
+            <i class="fa fa-check text-success"></i>
+        {{else}}
+            <i class="fa fa-times text-danger"></i>
+      {{/if}}{{/if}}
+
+      {{{question}}}
+
+      {{#if scored}}{{#if feedback}}
+        <span class={{#if correct}}feedback_correct{{else}}feedback_incorrect{{/if}}>
+          {{{feedback}}}
+        </span>
+      {{/if}}{{/if}}
+    </p>
     <p><input type="radio" name="{{code}}" {{#if value_true}}checked{{/if}} value="T"/> True
     <input type="radio" name="{{code}}" {{#if value_false}}checked{{/if}} value="F"/> False
     </p>
